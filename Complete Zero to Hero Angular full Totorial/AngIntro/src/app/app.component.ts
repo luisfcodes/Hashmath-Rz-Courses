8fee1682656bc62792060dae5a6fce27242f34d7
Luis Fernando <luis.silva9902@gmail.com>
2023-06-16 09:54:41 -0300

    Working with Event Binding and Filtering in Angular

--- Complete Zero to Hero Angular full Totorial/AngIntro/src/app/app.component.ts
+++ Complete Zero to Hero Angular full Totorial/AngIntro/src/app/app.component.ts
@@ -12,15 +12,29 @@ export class AppComponent implements AfterViewInit {
   parentMessage:string = 'Message coming from parent component';
   message!:string
   fromChildOutput!:string;
+  imgUrl:string = 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40'
+  textIsRed:boolean = true;
+  inputText:string | null = 'Valor inicial';
 
   @ViewChild(PostComponent) childComponent:any;
 
   ngAfterViewInit() {
-    console.log(this.childComponent);
     this.message = this.childComponent.childMessage;
   }
 
   receiveMessage($event: string) {
     this.fromChildOutput = $event;
+  }
+
+  eventBindingClick(){
+    alert('Event binding clicked');
+  }
+
+  eventKeyup($event: any){
+    this.inputText = $event.target.value;
+  }
+
+  eventFiltering(){
+    console.log('Enter key pressed');
   }
 }
