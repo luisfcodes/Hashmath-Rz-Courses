78c7061bfc2190815485fd5c46e66e0cb749880f
Luis Fernando <luis.silva9902@gmail.com>
2023-06-27 10:03:39 -0300

    Working with services and dependency injection, and creating typescript interface with Angular CLI

---
+++ Complete Zero to Hero Angular full Totorial/AngIntro/src/app/Services/post.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { Post } from "../models/post";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PostService {
+  postList: Post[] = [
+    { id: 1, postTitle: 'Post 1' },
+    { id: 2, postTitle: 'Post 2' },
+    { id: 3, postTitle: 'Post 3' },
+    { id: 4, postTitle: 'Post 4' },
+    { id: 5, postTitle: 'Post 5' },
+    { id: 6, postTitle: 'Post 6' },
+  ]
+
+  addPost(obj: Post) {
+    this.postList.push(obj);
+  }
+}
--- Complete Zero to Hero Angular full Totorial/AngIntro/src/app/post/post.component.ts
+++ Complete Zero to Hero Angular full Totorial/AngIntro/src/app/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PostService } from '../Services/post.service';
 
 @Component({
   selector: 'app-post',
@@ -11,10 +12,18 @@ export class PostComponent {
   childMessage:string = 'Message coming from child component';
   outputChildMessage: string = 'Message coming from child component Via Output';
 
+  postList = this.postService.postList;
+
+  constructor(private postService: PostService) { }
+
   @Input() fromParent!:string;
   @Output() messageEvent = new EventEmitter<string>();
 
   sendMessage() {
     this.messageEvent.emit(this.outputChildMessage);
+  }
+
+  addNewData() {
+    this.postService.addPost({id: 7, postTitle: 'Post 7'});
   }
 }
