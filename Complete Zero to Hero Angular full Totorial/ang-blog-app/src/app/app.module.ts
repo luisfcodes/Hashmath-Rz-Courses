0bb9de21272f446f9cba72bfb60583e2a52a410d
Luis Fernando <luis.silva9902@gmail.com>
2023-07-02 18:34:09 -0300

    Created new components and pages, installed bootstrap 4.6v and added css variables

--- Complete Zero to Hero Angular full Totorial/ang-blog-app/src/app/app.module.ts
+++ Complete Zero to Hero Angular full Totorial/ang-blog-app/src/app/app.module.ts
@@ -6,13 +6,31 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostsComponent } from './pages/single-posts/single-posts.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { SubscriptionFormComponent } from './components/subscription-form/subscription-form.component';
+import { CommentFormComponent } from './components/comments/comment-form/comment-form.component';
+import { CommentListComponent } from './components/comments/comment-list/comment-list.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CategoryNavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostsComponent,
+    TermsAndConditionComponent,
+    ContactUsComponent,
+    SubscriptionFormComponent,
+    CommentFormComponent,
+    CommentListComponent,
+    AboutUsComponent,
   ],
   imports: [
     BrowserModule,
